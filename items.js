0ecc4dd9ad5c1712725896721567a482aa326635
towles-osu <towles@oregonstate.edu>
2021-10-25 14:34:30 -0700

    items sample done

--- items.js
+++ items.js
@@ -1,12 +1,26 @@
 
-const sampleData = {
-
-};
+const sampleData = [
+    { name: "Magic Booster", sku: 1, quant: 50, price: 5 },
+    { name: "Dragon Statue", sku:2, quant:4, price:40.50}
+];
 
 document.addEventListener('DOMContentLoaded', initialize);
 
 
 //pulls basic info to display from database and inputs it
+function initDisplay() {
+    let displayArea = document.getElementById("outputArea");
+    let displayTable = document.createElement("table");
+    displayArea.append(displayTable);
+
+    //THIS NEEDS TO CHANGE WHEN WE ADD BACKEND
+    for (item in sampleData) {
+        console.log(item)
+        let newRow = document.createElement("tr");
+        newRow.innerText = sampleData[item].name;
+        displayTable.append(newRow);
+    }
+}
 
 //Initializes elements in html
 function initialize() {
