f808b650d72c69dd20ced7ff7c206290c81831e0
towles-osu <towles@oregonstate.edu>
2021-10-25 14:14:51 -0700

    wrote html for items

---
+++ items.js
@@ -0,0 +1,14 @@
+
+const sampleData = {
+
+};
+
+document.addEventListener('DOMContentLoaded', initialize);
+
+
+//pulls basic info to display from database and inputs it
+
+//Initializes elements in html
+function initialize() {
+    initDisplay();
+}
