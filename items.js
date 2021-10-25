4ae0d4e41a7f45f885a187bc58aeb8bb81073607
towles-osu <towles@oregonstate.edu>
2021-10-25 16:55:51 -0700

    finished items

--- items.js
+++ items.js
@@ -9,20 +9,123 @@ document.addEventListener('DOMContentLoaded', initialize);
 
 //pulls basic info to display from database and inputs it
 function initDisplay() {
-    let displayArea = document.getElementById("outputArea");
-    let displayTable = document.createElement("table");
-    displayArea.append(displayTable);
+    //let displayArea = document.getElementById("outputArea");
+    let displayTable = document.getElementById("displayTable");
+
 
     //THIS NEEDS TO CHANGE WHEN WE ADD BACKEND
     for (item in sampleData) {
         console.log(item)
         let newRow = document.createElement("tr");
-        newRow.innerText = sampleData[item].name;
+        let rowCol = document.createElement("td");
+        rowCol.innerText = sampleData[item].name;
+        newRow.append(rowCol);
+        rowCol = document.createElement("td");
+        rowCol.innerText = sampleData[item].sku;
+        newRow.append(rowCol);
+        rowCol = document.createElement("td");
+        rowCol.innerText = sampleData[item].quant;
+        newRow.append(rowCol);
+        rowCol = document.createElement("td");
+        rowCol.innerText = sampleData[item].price;
+        newRow.append(rowCol);
+        rowCol = document.createElement("td");
+        let button = document.createElement("input");
+        button.type = "button";
+        button.value = "edit";
+        rowCol.append(button);
+        button = document.createElement("input");
+        button.type = "button";
+        button.value = "delete";
+        rowCol.append(button);
+        newRow.append(rowCol);
         displayTable.append(newRow);
+    }
+}
+
+function clickCheck(event) {
+    //console.log(event);
+    if (event.srcElement.value == "edit") {
+        let col = event.srcElement.parentNode;
+
+        let row = col.parentNode;
+        let curBox = row.firstChild;
+        let newCon = document.createElement("input");
+        newCon.type = "text";
+        newCon.value = curBox.innerText;
+        curBox.innerText = "";
+        curBox.append(newCon);
+
+        curBox = curBox.nextSibling;
+        newCon = document.createElement("input");
+        newCon.type = "number";
+        newCon.style = "width: 30px";
+        newCon.value = curBox.innerText;
+        curBox.innerText = "";
+        curBox.append(newCon);
+
+        curBox = curBox.nextSibling;
+        newCon = document.createElement("input");
+        newCon.type = "number";
+        newCon.style = "width: 30px";
+        newCon.value = curBox.innerText;
+        curBox.innerText = "";
+        curBox.append(newCon);
+
+        curBox = curBox.nextSibling;
+        newCon = document.createElement("input");
+        newCon.type = "number";
+        newCon.style = "width: 30px";
+        newCon.value = curBox.innerText;
+        curBox.innerText = "";
+        curBox.append(newCon);
+
+        col.firstChild.remove();
+        col.firstChild.remove();
+        newCon = document.createElement("input");
+        newCon.type = "button";
+        newCon.value = "save";
+        col.append(newCon);
+    }
+    else if (event.srcElement.value == "save") {
+        let col = event.srcElement.parentNode;
+
+        let row = col.parentNode;
+        let curBox = row.firstChild;
+        let newCon = curBox.firstChild;
+        curBox.innerText = newCon.value;
+        newCon.remove();
+
+        curBox = curBox.nextSibling;
+        newCon = curBox.firstChild;
+        curBox.innerText = newCon.value;
+        newCon.remove();
+
+        curBox = curBox.nextSibling;
+        newCon = curBox.firstChild;
+        curBox.innerText = newCon.value;
+        newCon.remove();
+
+        curBox = curBox.nextSibling;
+        newCon = curBox.firstChild;
+        curBox.innerText = newCon.value;
+        newCon.remove();
+
+        col.firstChild.remove();
+        let button = document.createElement("input");
+        button.type = "button";
+        button.value = "edit";
+        col.append(button);
+        button = document.createElement("input");
+        button.type = "button";
+        button.value = "delete";
+        col.append(button);
+        
     }
 }
 
 //Initializes elements in html
 function initialize() {
     initDisplay();
+    document.addEventListener('click', clickCheck);
 }
