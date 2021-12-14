ae4b59bc542e0dd2bc38c648e2341371e3ee0c01
Stew <71574207+towles-osu@users.noreply.github.com>
2021-12-14 14:43:46 -0800

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -1,11 +1,12 @@
 //sql database connection setup
+//NEEDS TO BE REDONE WHEN SETTING UP NEW DATABSE
 
 const mysql = require('mysql');
 const pool = mysql.createPool({
     connectionLimit : 10,
     host : 'classmysql.engr.oregonstate.edu',
     user : 'cs340_towles',
-    password : '8494',
+    password : '',//LEFT BLANK FOR SECURITY REASONS
     database : 'cs340_towles'
 });
 
